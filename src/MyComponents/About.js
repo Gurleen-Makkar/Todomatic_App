30a311ef2151111ee96bc4cd34484f5a44fb2117
Gurleen <gurleenmakkar1@gmail.com>
2022-07-02 02:12:23 +0530

    final

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about Component!
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quos ea porro laudantium rerum, magni impedit. Illo, sunt. Soluta, quia placeat corporis quas consequuntur unde beatae. Provident praesentium quod eveniet?</p>
+    </div>
+  )
+}
+
+export default About
